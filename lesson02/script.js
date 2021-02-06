f9d7bfc7501d41711ee4911a910d527a99745376
Dmitry Avilov <d.av89@yandex.ru>
2021-02-07 00:50:56 +0300

    complete lesson 2

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,7 +1,19 @@
 let money = 150000,
-    income = 120000,
-    addExpenses = "интернет, такси, коммуналка",
+    income = "120000",
+    addExpenses = "Интернет, Такси, Коммуналка",
     deposit = true,
     mission = 12000000,
     period = 6;
 
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " долларов");
+
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(", "));
+
+let budgetDay = 30;
+console.log(budgetDay);
