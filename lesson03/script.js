a0523f55d3a395960d6ee0071fa01f247650091e
Dmitry Avilov <d.av89@yandex.ru>
2021-02-09 05:01:21 +0300

    complete lesson 03

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,12 +1,19 @@
 'use strict';
 
-let money = 150000,
+let money = prompt('Ваш месячный доход?'),
     income = "120000",
-    addExpenses = "Интернет, Такси, Коммуналка",
-    deposit = true,
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    expenses1 = prompt('Введите обязательную статью расходов?'),
+    expenses2 = prompt('Введите обязательную статью расходов?'),
+    amount1 = prompt('Во сколько это обойдется?'),
+    amount2 = prompt('Во сколько это обойдется?'),
     mission = 12000000,
+    budgetMonth = +money - +expenses1 - +expenses2 - +amount1 - +amount2,
+    budgetDay = parseInt(budgetMonth / 30),
     period = 6;
 
+console.log('Месячный бюджет: ', budgetMonth);
 console.log(typeof(money));
 console.log(typeof(income));
 console.log(typeof(deposit));
@@ -16,6 +23,20 @@ console.log("Цель заработать " + mission + " долларов");
 
 addExpenses = addExpenses.toLowerCase();
 console.log(addExpenses.split(", "));
+console.log('Дневной бюджет: ', budgetDay);
 
-let budgetDay = money / 30;
-console.log(budgetDay);
+switch (true) {
+    case budgetDay >= 1200:
+        console.log('У вас высокий уровень дохода');
+        break;
+    case budgetDay === 600:
+    case budgetDay > 600 && budgetDay < 1200:
+        console.log('У вас средний уровень дохода');
+        break;
+    case budgetDay === 0:
+    case budgetDay > 0 && budgetDay < 600:
+        console.log('У вас низкий уровень дохода');
+        break;
+    case budgetDay < 0:
+        console.log('Что-то пошло не так');
+}
