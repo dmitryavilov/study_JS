2bfa7efe5f0f70b05ff73cb1aeba9c3a868ce46c
Dmitry Avilov <d.av89@yandex.ru>
2021-02-09 19:39:34 +0300

    lesson 03 fix

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let money = prompt('Ваш месячный доход?'),
+let money = +prompt('Ваш месячный доход?'),
     income = "120000",
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     expenses1 = prompt('Введите обязательную статью расходов?'),
     expenses2 = prompt('Введите обязательную статью расходов?'),
-    amount1 = prompt('Во сколько это обойдется?'),
-    amount2 = prompt('Во сколько это обойдется?'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+    amount2 = +prompt('Во сколько это обойдется?'),
     mission = 12000000,
-    budgetMonth = +money - +expenses1 - +expenses2 - +amount1 - +amount2,
+    budgetMonth = money - amount1 - amount2,
     budgetDay = parseInt(budgetMonth / 30),
     period = 6;
 
