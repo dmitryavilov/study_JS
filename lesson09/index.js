480cf979faa75caa26cbe26fca22b83db89ee3de
Dmitry Avilov <d.av89@yandex.ru>
2021-02-16 21:45:28 +0300

    complete lesson09

---
+++ lesson09/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const finalPrice = document.getElementById('start'),
+      plusButtonOne = document.querySelectorAll('.btn_plus')[0],
+      plusButtonTwo = document.querySelectorAll('.btn_plus')[0],
+      depositCheckBox = document.querySelector('#deposit-check'),
+      additionalIncomes = document.querySelectorAll('.additional_income-item'),
+      rightSide = document.querySelector('.result'),
+      valuesItems = rightSide.querySelectorAll('[class*=-value]'),
+      salaryAmount = document.querySelector('.salary-amount'),
+      incomeTitle = document.querySelector('.income-title'),
+      incomeAmount = document.querySelector('.income-amount'),
+      additionalIncomeItem1 = document.querySelectorAll('.additional_income-item')[0],
+      additionalIncomeItem2 = document.querySelector('.additional_income-item')[1],
+      expensesTitle = document.querySelector('.expenses-title'),
+      expensesAmount = document.querySelector('.expenses-amount'),
+      additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+      depositAmount = document.querySelector('.deposit-amount'),
+      depositPercent = document.querySelector('.deposit-percent'),
+      targetAmount = document.querySelector('.target-amount'),
+      periodSelect = document.querySelector('.period-select');
+
