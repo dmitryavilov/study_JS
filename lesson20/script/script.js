b140de7e87cb98f4b6b4081d3f3fd57c129a2b3c
Dmitry Avilov <d.av89@yandex.ru>
2021-03-04 07:45:21 +0300

    fix

--- lesson20/script/script.js
+++ lesson20/script/script.js
@@ -94,8 +94,8 @@ window.addEventListener('DOMContentLoaded', () => {
             int;
 
         const animatePopup = () => {
-            if (opacityCounter < 1) {
-                popup.style.display = 'block';
+            popup.style.display = 'block';
+            if (opacityCounter < 1 && document.documentElement.clientWidth > 768) {
                 opacityCounter += 0.01;
                 popup.style.opacity = opacityCounter;
             } else {
@@ -105,13 +105,18 @@ window.addEventListener('DOMContentLoaded', () => {
         
         popupBtn.forEach(item => {
             item.addEventListener('click', animatePopup);
-            item.addEventListener('click', () => int = setInterval(animatePopup, 8));
+
+            if (document.documentElement.clientWidth > 768) {
+                item.addEventListener('click', () => int = setInterval(animatePopup, 8));
+            };
         });
 
         popupClose.addEventListener('click', () => {
             popup.style.display = 'none';
-            opacityCounter = 0; 
-            clearInterval(int);
+            if (document.documentElement.clientWidth > 768) {
+                opacityCounter = 0; 
+                clearInterval(int);
+            }
         });
 
         popup.addEventListener('click', e => {
