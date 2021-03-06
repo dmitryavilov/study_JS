9add509f20d615c11693011977c2ec6f2bf0e03b
Dmitry Avilov <d.av89@yandex.ru>
2021-03-06 06:15:06 +0300

    first part completed

--- lesson23/script/script.js
+++ lesson23/script/script.js
@@ -271,8 +271,29 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
         startSlide(2500);
-
     };
 
     slider();
+
+    //Command
+
+    const ourCommand = () => {
+        const command = document.getElementById('command');
+
+        command.addEventListener('mouseover', e => {
+            let target = e.target;
+
+            if (target.matches('.command__photo')) {
+                target.dataset.back = target.src
+                target.src = target.dataset.img;
+
+                target.addEventListener('mouseout', () => {
+                    target.src = target.dataset.back;
+                    target.removeEventListener();
+                });
+            };
+        });
+    };
+
+    ourCommand();
 });
